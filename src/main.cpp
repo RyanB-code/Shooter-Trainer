afaa080436e9485c426e837fd24e09efa029b779
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-04-02 13:02:33 -0700

    Finished lesson 2 about loading PNGs

--- src/main.cpp
+++ src/main.cpp
@@ -5,92 +5,181 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3_image/SDL_image.h>
 
-class Window {
+// constants
+constexpr int kScreenWidth 	{ 640 };
+constexpr int kScreenHeight 	{ 800 }; 
+
+class LTexture;
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Renderer* 	gRenderer 	{ nullptr };
+SDL_Window* 	gWindow		{ nullptr };
+
+
+// Class definitions
+class LTexture
+{
 public:
-	Window(std::string setTitle="Test Window") : title {setTitle} { }
-	~Window() { 
+	//Initializes texture variables
+ 	LTexture(){
+		mTexture = nullptr;
+		mWidth = 0;
+		mHeight = 0;
+	}
+
+   	//Cleans up texture variables
+	~LTexture(){
 		destroy();
 	}
 
-	bool init(int height, int width){
-		if(!SDL_Init(SDL_INIT_VIDEO) ){
-			SDL_Log("SDL could not initialize. SDL Error: %s\n", SDL_GetError());
-			return false;
-		}
+    	//Loads texture from disk
+    	bool loadFromFile( std::string path ){
+		destroy(); // Cleans texture if already in use
 		
-		window = SDL_CreateWindow(title.c_str(), width, height, 0);
-		
-		if(!window){
-			SDL_Log("Window could not be created. SDL Error: %s\n", SDL_GetError());
+		SDL_Surface* loadedSurface { nullptr };
+		loadedSurface = IMG_Load(path.c_str());
+
+		if(!loadedSurface){
+			SDL_Log("Unable to load image %s. SDL Error: %s\n", path.c_str(), SDL_GetError());
+			SDL_DestroySurface(loadedSurface);
 			return false;
 		}
 
-		surface = SDL_GetWindowSurface( window );
-	
-		return true;
+
+		if(mTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface); mTexture == nullptr){
+			SDL_DestroySurface(loadedSurface);
+			SDL_Log("Unable to create texture from loaded pixels. SDL_Error %s\n", SDL_GetError());
+			return false;
+		}
+
+		mWidth = loadedSurface->w;
+		mHeight = loadedSurface->h;
+
+		SDL_DestroySurface(loadedSurface); // Cleanup local variable
+
+		return mTexture != nullptr;
 	}
-	void destroy(){
-		SDL_DestroyWindow(window);
-		window 	= nullptr;
-		surface = nullptr;
+
+  	//Cleans up texture
+    	void destroy(){
+		SDL_DestroyTexture(mTexture);
+		mTexture = nullptr;
+		mWidth = 0;
+		mHeight = 0;
 	}
-	
-	SDL_Window* 	getWindowPtr() 	{ return window; };
-	SDL_Surface*	getSurfacePtr()	{ return surface; };
+
+	//Draws texture
+    	void render( float x, float y ){
+		// Set texture position
+		SDL_FRect dstRect = {x, y, static_cast<float>(mWidth), static_cast<float>(mHeight)};
+
+		SDL_RenderTexture(gRenderer, mTexture, nullptr, &dstRect); // Render it
+	}
+
+    	//Gets texture dimensions
+    	int getWidth()	{ return mWidth; };
+    	int getHeight()	{ return mHeight; };
 
 private:
-	SDL_Window* 	window;
-	SDL_Surface*	surface;
-	std::string title;
+    	//Contains texture data
+    	SDL_Texture* mTexture;
+
+    	//Texture dimensions
+    	int mWidth;
+    	int mHeight;
 };
 
+
+
+
+LTexture 	gPNGTexture;
+
 int main(){
-	std::cout << "Hello, world!\n";
-	std::cout << "Test 4\n";
 
-	constexpr int height 	{ 640 };
-	constexpr int width	{ 800 };
+	int exitCode { 0 };
 
-	Window window;
-
-	if(!window.init(height, width))
-		return false;
-
-	SDL_Surface* 	testSurface 	{ nullptr };
-
-
-	std::string imagePath { "bin/sdl-test_round.bmp" };
-	if(testSurface = SDL_LoadBMP(imagePath.c_str()); testSurface == nullptr){
-		SDL_Log("Unable to load image. SDL Error: %s\n", SDL_GetError());
-		return false;
+	if(!init()){
+		SDL_Log("Unable to initialize program!\n");
+		return 1;
 	}
 
-	// Getting input
+	if(!loadMedia()){
+		SDL_Log("Unable to load media\n");
+		return 2;
+	}
+
 	bool quit { false };
 
 	SDL_Event e;
-	SDL_zero( e );
+	SDL_zero(e);
 
-	while(quit == false){
+	while(!quit){
 		while(SDL_PollEvent(&e)){
 			if(e.type == SDL_EVENT_QUIT)
 				quit = true;
 		}
 
-		SDL_FillSurfaceRect(window.getSurfacePtr(), nullptr, SDL_MapSurfaceRGB(window.getSurfacePtr(), 0xFF, 0xFF, 0xFF) );
+		// fill background white
+		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_RenderClear(gRenderer);
 
-		SDL_BlitSurface(testSurface, nullptr, window.getSurfacePtr(), nullptr);
-
-		SDL_UpdateWindowSurface(window.getWindowPtr());
+		gPNGTexture.render(0.f, 0.f); 	// Render image on screen
+		SDL_RenderPresent(gRenderer);	// Update screen
 	}
 
-	
-	SDL_DestroySurface(testSurface);
-	testSurface = nullptr;
-
-	window.destroy();
-
-	SDL_Quit();
-
-	return 0;
+	close();
+	return exitCode;
 }
+
+
+// Function definitions
+bool init(){
+
+	if(!SDL_Init(SDL_INIT_VIDEO)){
+		SDL_Log("SDL could not initialize. SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	// Create window with renderer
+	if(!SDL_CreateWindowAndRenderer("Test Text", kScreenWidth, kScreenHeight, 0, &gWindow, &gRenderer)){
+		SDL_Log("Window could not be created. SDL Error %s\n", SDL_GetError());
+		return false;
+	}
+
+	/*
+	// Initialize PNG loading
+	int imgFlags = IMG_INIT_PNG;
+	if(!(IMG_Init(imgFlags) & imgFlags)){
+		SDL_Log("SDL_image could not initialize. SDL_image error: %s\n", SDL_GetError());
+		return false;
+	}
+	*/
+
+	return true;
+}
+bool loadMedia(){
+	bool success { true };
+	if(success = gPNGTexture.loadFromFile("bin/thumbnail.png"); !success){
+		SDL_Log("Unable to load png image");
+	}
+
+	return success;
+}
+void close(){
+	gPNGTexture.destroy();
+
+	// Destroy window
+	SDL_DestroyRenderer(gRenderer);
+	gRenderer = nullptr;
+	SDL_DestroyWindow(gWindow);
+	gWindow = nullptr;
+
+	// Quit subsystems
+	//IMG_Quit();
+	SDL_Quit();
+}
+
+
