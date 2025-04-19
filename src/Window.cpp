77a9615b36c33365ff0177d8094f28429d173db1
Bradley Ryan <ryan.bradley.e@gmail.com>
2025-04-19 15:26:37 -0700

    Added event loop and window flags

--- src/Window.cpp
+++ src/Window.cpp
@@ -23,5 +23,30 @@ bool RangeSession::init(SDL_Window* setWindow, SDL_Renderer* setRenderer) {
         return renderer && window; // Perform check on renderer to see if made properly and return
     }
 
-    return SDL_CreateWindowAndRenderer(TITLE, WIDTH, HEIGHT, 0, &window, &renderer);
+    return SDL_CreateWindowAndRenderer(TITLE, WIDTH, HEIGHT, FLAGS, &window, &renderer);
 }
+void RangeSession::run(){
+    if(!window || !renderer)
+        throw std::runtime_error("Must initialize before running");
+
+    bool quit { false };
+
+    SDL_Event e;
+    SDL_zero(e);
+
+    while(!quit){
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_EVENT_QUIT)
+                quit = true;
+        }
+                
+        //Fill the background 
+        SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear( renderer );
+
+        // Update renderer
+        SDL_RenderPresent(renderer);
+    }
+}
+
+
--- src/Window.h
+++ src/Window.h
@@ -11,14 +11,16 @@ namespace ShooterTrainer{
         ~RangeSession();
 
         bool init(SDL_Window* window=nullptr, SDL_Renderer* renderer=nullptr);
+        void run(); // Throw runtime error if called before initializing
 
     private:
         static constexpr char   TITLE[] { "Shooter Trainer" };
         static constexpr int    WIDTH   { 640 };
         static constexpr int    HEIGHT  { 800 };
 
+        static constexpr SDL_WindowFlags FLAGS { SDL_WINDOW_RESIZABLE };
+
         SDL_Window*     window      { nullptr };
         SDL_Renderer*   renderer    { nullptr };
     };
-
 }
