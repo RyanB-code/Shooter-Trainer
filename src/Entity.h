45001ba1d1cff2d7ea517fcb23283303f9c042ef
Bradley Ryan <ryan.bradley.e@gmail.com>
2025-05-04 19:05:30 -0700

    Added a base entity class that contains a UUID and a registry for components

--- src/Entity.h
+++ src/Entity.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Registry.h"
 
 #include <random>
 
@@ -15,7 +16,39 @@ namespace ShooterTrainer {
         uint64_t id;
     };
 
-};
+    class Entity{
+    public:
+        Entity();
+        Entity(UUID setId);
+        ~Entity();
+
+        UUID getUUID() const;
+
+        std::size_t getRegistrySize() const;
+
+        // Registry Interaction
+        template<typename T>
+        bool addComponent(T v){
+            return registry.addComponent(v);
+        }
+        template<typename T>
+        void removeComponent(T v){
+            return registry.removeComponent(v);
+        }
+        template<typename T>
+        bool hasComponenet(T v) const {
+            return registry.hasComponent(v);
+        }
+        template<typename T>
+        T& getComponent(T v) {
+            return registry.getComponent(v);
+        }
+
+    private:
+        EntityRegistry registry { };
+        UUID id;
+    }; 
+}
 
 namespace std{
     template<>
