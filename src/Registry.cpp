4c4c014d23c3012548131de4043dfa687be0b6bc
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-05-04 17:34:06 -0700

    Added a registry in the form of an unordered_map for adding components to an entity

---
+++ src/Registry.cpp
@@ -0,0 +1,22 @@
+#include <Registry.h>
+
+
+using namespace ShooterTrainer;
+
+std::size_t EntityTypeHasher::operator()(TypeInfoRef type) const{
+    return type.get().hash_code();
+}
+
+bool EntityTypeEquivalence::operator()(TypeInfoRef lhs, TypeInfoRef rhs) const{
+    return lhs.get() == rhs.get();
+}
+
+EntityRegistry::EntityRegistry(){
+
+}
+EntityRegistry::~EntityRegistry(){
+
+}
+
+
+
