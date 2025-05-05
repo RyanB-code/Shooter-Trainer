272a0badf007a878ac24154fd4a79688e8402dfd
Bradley Ryan <ryan.bradley.e@gmail.com>
2025-05-04 18:16:04 -0700

    Added UUID abilities for an Entity

---
+++ src/Entity.cpp
@@ -0,0 +1,24 @@
+#include "Entity.h"
+
+
+namespace ShooterTrainer{
+
+static std::random_device RANDOM_DEVICE;
+static std::mt19937_64 RANDOM_ENGINE(RANDOM_DEVICE());
+static std::uniform_int_distribution<uint64_t> UNIFORM_DISTRIBUTION;
+
+UUID::UUID() : id { UNIFORM_DISTRIBUTION(RANDOM_ENGINE) }{
+
+}
+UUID::UUID(uint64_t setID) : id { setID } {
+
+}
+UUID::~UUID(){
+
+}
+UUID::operator uint64_t() const {
+    return id;
+}
+    
+
+}
