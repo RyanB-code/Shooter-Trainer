529e0d0f4e9cc3196478c6a87a546388a5465f40
Bradley Ryan <59941443+RyanB-code@users.noreply.github.com>
2025-05-17 19:48:13 -0700

    Adding WIP items

--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -2,7 +2,11 @@
 
 #include "Framework.h"
 
+// For testing things quickly
+#include "Entity.h"
+
 using namespace ShooterTrainer::Framework;
+
 
 int main() {
     Framework application;
@@ -14,3 +18,39 @@ int main() {
 
     return 0;
 }
+
+
+/*
+int main() {
+    using namespace ShooterTrainer;
+
+    Entity fuck;
+
+    std::cout << "UUID: " << fuck.getUUID() << "\n";
+
+    bool test1 { true };
+    std::string test2 { "test text" };
+
+    fuck.addComponent(test1);
+    fuck.addComponent(test2);
+
+    std::cout << "Reg Size: " << fuck.getRegistrySize() << "\n";
+
+    fuck.removeComponent(test1);
+
+    std::cout << "Reg Size Again: " << fuck.getRegistrySize() << "\n";
+
+    auto ref = fuck.getComponent(test2);
+
+    std::cout << "Text 1: " << ref << "\n";
+
+    ref = "new text";
+
+    auto& ref2 = fuck.getComponent(std::string {});
+
+    std::cout << "Text 2: " << ref2 << "\n";
+
+
+    return 0;
+}
+*/
--- src/Window.h
+++ src/Window.h
@@ -1,26 +1,92 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+
+#include <stdexcept>
+#include <string>
+#include <memory>
 
 #include <iostream>
 
+struct Vec2 {
+    float x { 0.f };
+    float y { 0.f };
+}; 
+
+std::ostream& operator<<(std::ostream& os, const Vec2& v);
+
 namespace ShooterTrainer{
 
+    struct AspectRatio {
+        Vec2 scale { 0, 0 };
+        Vec2 logicalSize { 0, 0 };
+    };
+
+    static constexpr AspectRatio ASPECT_RATIOS[] = { 
+        { { 4, 3    }, { 1024, 768  } },
+        { { 16, 9   }, { 1920, 1080 } },
+        { { 16, 10  }, { 1920, 1200 } },
+        { { 21, 9   }, { 2560, 1080 } }
+    };
+
+    struct WindowSettings {
+        const char* title       { nullptr };    // Set window title
+        bool        fullscreen  { false };      // Set if window will open in fullscreen mode
+        bool        resizable   { true };       // Set if window can be resized
+        Vec2        size        { 800, 640 };   // Set first window size
+        AspectRatio aspectRatio { ASPECT_RATIOS[1] };
+    };
+    
+    class Window {
+    public:
+        Window();
+        ~Window(); 
+
+        bool init   (WindowSettings setSettings);
+        bool isInit () const;
+
+        void destroy ();
+
+        const WindowSettings&   getSettings() const;
+        void                    setSettings(const WindowSettings& setSettings);
+        
+        SDL_Renderer* getRenderer() const;
+    private:
+        bool            initialized { false };
+
+        WindowSettings  settings    { };
+        SDL_Window*     window      {nullptr};
+        SDL_Renderer*   renderer    {nullptr};
+    };
+
+    using WindowPtr = std::shared_ptr<Window>;
+
+   
     class RangeSession {
     public:
         RangeSession();
         ~RangeSession();
 
-        bool init(SDL_Window* window=nullptr, SDL_Renderer* renderer=nullptr);
+        bool init(WindowPtr setWindow=nullptr); // Must be an already initialized window if giving one
         void run(); // Throw runtime error if called before initializing
 
     private:
-        static constexpr char   TITLE[] { "Shooter Trainer" };
-        static constexpr int    WIDTH   { 640 };
-        static constexpr int    HEIGHT  { 800 };
+        WindowPtr window;
+    };
 
-        static constexpr SDL_WindowFlags FLAGS { SDL_WINDOW_RESIZABLE };
+    class Texture {
+    public:
+        Texture();
+        ~Texture();
 
-        SDL_Window*     window      { nullptr };
-        SDL_Renderer*   renderer    { nullptr };
+        int load(const std::string& path, SDL_Renderer* renderer);          // Return 1: Unable to load image. 2: Unable to color key. 3: Unable to create texture
+        
+        void render(SDL_Renderer* renderer, Vec2 pos, SDL_FRect* clip=nullptr, Vec2 clipSize={-1, -1});
+
+        void destroy();
+    private:
+        SDL_Texture*    texture         { nullptr };
+        Vec2            originalSize    { 0, 0 };
+        Vec2            center          { 0, 0 };
     };
 }
